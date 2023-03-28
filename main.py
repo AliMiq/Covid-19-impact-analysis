fb067e209a4c9e66a1502166249144863895be01
Ali <raialihassanshahbaz123456@gmail.com>
2023-03-28 12:29:17 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,134 @@
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+
+data = pd.read_csv("transformed_data.csv")
+data2 = pd.read_csv("raw_data.csv")
+print(data)
+
+print(data2.head())
+
+data["COUNTRY"].value_counts()
+
+data["COUNTRY"].value_counts().mode()
+
+# Aggregating the data
+
+code = data["CODE"].unique().tolist()
+country = data["COUNTRY"].unique().tolist()
+hdi = []
+tc = []
+td = []
+sti = []
+population = data["POP"].unique().tolist()
+gdp = []
+
+for i in country:
+    hdi.append((data.loc[data["COUNTRY"] == i, "HDI"]).sum()/294)
+    tc.append((data2.loc[data2["location"] == i, "total_cases"]).sum())
+    td.append((data2.loc[data2["location"] == i, "total_deaths"]).sum())
+    sti.append((data.loc[data["COUNTRY"] == i, "STI"]).sum()/294)
+    population.append((data2.loc[data2["location"] == i, "population"]).sum()/294)
+
+aggregated_data = pd.DataFrame(list(zip(code, country, hdi, tc, td, sti, population)), 
+                               columns = ["Country Code", "Country", "HDI", 
+                                          "Total Cases", "Total Deaths", 
+                                          "Stringency Index", "Population"])
+print(aggregated_data.head())
+
+# Sorting Data According to Total Cases
+
+data = aggregated_data.sort_values(by=["Total Cases"], ascending=False)
+print(data.head())
+
+# Top 10 Countries with Highest Covid Cases
+
+data = data.head(10)
+print(data)
+
+data["GDP Before Covid"] = [65279.53, 8897.49, 2100.75, 
+                            11497.65, 7027.61, 9946.03, 
+                            29564.74, 6001.40, 6424.98, 42354.41]
+data["GDP During Covid"] = [63543.58, 6796.84, 1900.71, 
+                            10126.72, 6126.87, 8346.70, 
+                            27057.16, 5090.72, 5332.77, 40284.64]
+print(data)
+
+figure = px.bar(data, y='Total Cases', x='Country',
+            title="Countries with Highest Covid Cases")
+figure.show()
+
+figure = px.bar(data, y='Total Cases', x='Country',
+            title="Countries with Highest Covid Cases")
+figure.show()
+
+fig = go.Figure()
+fig.add_trace(go.Bar(
+    x=data["Country"],
+    y=data["Total Cases"],
+    name='Total Cases',
+    marker_color='indianred'
+))
+fig.add_trace(go.Bar(
+    x=data["Country"],
+    y=data["Total Deaths"],
+    name='Total Deaths',
+    marker_color='lightsalmon'
+))
+fig.update_layout(barmode='group', xaxis_tickangle=-45)
+fig.show()
+
+# Percentage of Total Cases and Deaths
+cases = data["Total Cases"].sum()
+deceased = data["Total Deaths"].sum()
+
+labels = ["Total Cases", "Total Deaths"]
+values = [cases, deceased]
+
+fig = px.pie(data, values=values, names=labels, 
+             title='Percentage of Total Cases and Deaths', hole=0.5)
+fig.show()
+
+death_rate = (data["Total Deaths"].sum() / data["Total Cases"].sum()) * 100
+print("Death Rate = ", death_rate)
+
+fig = px.bar(data, x='Country', y='Total Cases',
+             hover_data=['Population', 'Total Deaths'], 
+             color='Stringency Index', height=400, 
+             title= "Stringency Index during Covid-19")
+fig.show()
+
+fig = px.bar(data, x='Country', y='Total Cases',
+             hover_data=['Population', 'Total Deaths'], 
+             color='GDP Before Covid', height=400, 
+             title="GDP Per Capita Before Covid-19")
+fig.show()
+
+fig = px.bar(data, x='Country', y='Total Cases',
+             hover_data=['Population', 'Total Deaths'], 
+             color='GDP During Covid', height=400, 
+             title="GDP Per Capita During Covid-19")
+fig.show()
+
+fig = go.Figure()
+fig.add_trace(go.Bar(
+    x=data["Country"],
+    y=data["GDP Before Covid"],
+    name='GDP Per Capita Before Covid-19',
+    marker_color='indianred'
+))
+fig.add_trace(go.Bar(
+    x=data["Country"],
+    y=data["GDP During Covid"],
+    name='GDP Per Capita During Covid-19',
+    marker_color='lightsalmon'
+))
+fig.update_layout(barmode='group', xaxis_tickangle=-45)
+fig.show()
+
+fig = px.bar(data, x='Country', y='Total Cases',
+             hover_data=['Population', 'Total Deaths'], 
+             color='HDI', height=400, 
+             title="Human Development Index during Covid-19")
+fig.show()
+
